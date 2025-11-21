65d9ba66281f7933d9aa1d77c3fea3026458b4b0
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-21 11:05:33 +0300

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://real-time-chat-app-eeii.onrender.com" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chat-application-13gl.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
